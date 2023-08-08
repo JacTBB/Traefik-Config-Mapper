7601b1bf77642661e47ba81ecb41b110350138a1
JacTBB <jackiesoon66@gmail.com>
2023-08-08 07:07:17 +0000

    Network update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.listen(port, () => {
 
 async function GetTraefikConfig() {
     let Traefik_Coolify
-    await fetch('http://coolify:3000/webhooks/traefik/main.json')
+    await fetch('http://0.0.0.0:3000/webhooks/traefik/main.json')
     .then(response => response.json())
     .then(data => {
         Traefik_Coolify = data
